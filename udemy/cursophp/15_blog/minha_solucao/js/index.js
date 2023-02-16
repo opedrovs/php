c6000601b156d35e4e31b2daa266b9922e6dfd06
= <92165034+pvsanches@users.noreply.github.com>
2023-02-16 17:34:45 -0300

    adicionando novas e melhores configurações no projeto blog

--- udemy/cursophp/15_blog/minha_solucao/js/index.js
+++ udemy/cursophp/15_blog/minha_solucao/js/index.js
@@ -1,20 +1,20 @@
-const burguer = document.querySelector('.burguer');
-const navList = document.querySelector('.nav-list');
-
-burguer.addEventListener('click', () => {
-    if(navList.style.display === 'block') {
-        navList.style.display = 'none';
-    } else {
-        navList.style.display = 'block';
-    }
-});
-
-// body.onresize()
-
-window.addEventListener('resize', () => {
-    if(window.innerWidth >= 765) {
-        navList.style.display = 'block';
-    } else {
-        navList.style.display = 'none';
-    }
-});
+(function() {
+    const burguer = document.querySelector('.burguer');
+    const navbar = document.querySelector('.navbar');
+    
+    burguer.addEventListener('click', () => {
+        if(navbar.style.display === 'block') {
+            navbar.style.display = 'none';
+        } else {
+            navbar.style.display = 'block';
+        }
+    });
+    
+    window.addEventListener('resize', () => {
+        if(window.innerWidth >= 765) {
+            navbar.style.display = 'block';
+        } else {
+            navbar.style.display = 'none';
+        }
+    });
+})();
